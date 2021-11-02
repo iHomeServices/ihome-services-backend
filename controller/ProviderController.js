e4e34b578ce0ea1835513b8e1bcd36addfa6fe52
Jose Rezende <zerezende12345@gmail.com>
2021-11-02 17:33:32 -0300

    Add endpoints to upload avatar and get avatar

--- controller/ProviderController.js
+++ controller/ProviderController.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var providerRouter = express.Router();
 const providerService = require('../service/providerService');
+const multer = require('multer');
+const fs = require('fs');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'controller/avatar')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage })
 
 providerRouter.get('/', async (req, res) => {
     const providers = await providerService.getProviders();
@@ -13,18 +26,40 @@ providerRouter.get('/:id', async (req, res) => {
 });
 
 providerRouter.post('/service', async (req, res) => {
-    // await uploadImage(req, res);
-
     const service = req.body;
     const result = providerService.createService(service);
-    res.status(200).send();
+    res.status(200).send(result);
 });
 
 providerRouter.put('/edit/:id', async (req, res) => {
-    const provider = req.body;
+    let provider = req.body;
     providerService.editProvider(req.params.id, provider);
     res.status(200).send();
 });
+
+providerRouter.post('/upload-avatar', upload.single('avatar'), (req, res, next) => {
+    const file = req.file;
+    res.send(file);
+});
+
+providerRouter.get('/avatar/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const path = __dirname + '/avatar/' + filename;
+    res.sendFile(path);
+});
+
+// providerRouter.put('/edit/:id', upload.single('avatar'), async (req, res) => {
+//     var img = fs.readFileSync(req.file.path);
+//     var encode_image = img.toString('base64');
+//     var finalImg = {
+//         contentType: req.file.mimetype,
+//         image: Buffer.from(encode_image, 'base64')
+//     }
+//     let provider = req.body;
+//     provider.avatar = finalImg;
+//     providerService.editProvider(req.params.id, provider);
+//     res.status(200).send();
+// });
 
 providerRouter.post('/finish-service/:id', async (req, res) => {
     const evaluation = req.body
@@ -35,23 +70,6 @@ providerRouter.post('/finish-service/:id', async (req, res) => {
 providerRouter.delete('/cancel-service/:id', async (req, res) => {
     providerService.cancelService(req.params.id);
     res.status(200).send();
-});
-
-providerRouter.post('/upload-avatar', (req, res, next) => {
-    const formidable = require('formidable');
-    const fs = require('fs');
-    const form = new formidable.IncomingForm();
-
-    form.parse(req, (err, fields, files) => {
- 
-        console.log('files', files);
-        const path = require('path');
-        const oldpath = files.filetoupload.filepath;
-        const newpath = path.join(__dirname, 'public', 'files');
-        
-        fs.renameSync(oldpath, newpath);
-        res.send('File uploaded and moved!');
-      });
 });
 
 module.exports = providerRouter;
